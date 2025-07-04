12fc2af5afc4fc7f59fa6db543b3c1e126e453a8
angchinsiang <angchinsiang38@gmail.com>
2025-07-04 16:37:56 +0800

    Create a Game Hook

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,32 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../Services/apiClient";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGame from "../Hooks/useGame";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  },[]);
-
+  const { games, error } = useGame();
   return (
     <>
       {error && <Text>{error}</Text>}
